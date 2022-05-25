8b6d71e9f31fdba5528817153013bf631e067b43
Aaketk17 <thavanthya@gmail.com>
2022-05-25 22:01:03 +0530

    changed

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import React from 'react'
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+} from 'react-native'
 
-export function HomeScreen(props: any) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 20 }}>Home Screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => {
-                    props.navigation.navigate('Details', {
-                        userId: 1,
-                        userName: 'Awesome User',
-                    });
-                }
-                }
-            />
-        </View>
-    );
+export function HomeScreen(props) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{fontSize: 20}}>
+        Home Screen
+      </Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate('Details', {
+            userId: 1,
+            userName: 'Awesome User',
+          })
+        }}
+      />
+    </View>
+  )
 }
--- src/TabNavigator/TabNavigator.js
+++ src/TabNavigator/TabNavigator.js
@@ -1,17 +1,28 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
-import { MainStackNavigator, ContactStackNavigator } from "./src/StackNavigator/StackNavigator";
+import {
+  MainStackNavigator,
+  ContactStackNavigator,
+} from './src/StackNavigator/StackNavigator'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={MainStackNavigator} />
-      <Tab.Screen name="Contact" component={ContactStackNavigator} />
+      <Tab.Screen
+        name="Home"
+        component={MainStackNavigator}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={ContactStackNavigator}
+      />
     </Tab.Navigator>
-  );
-};
+  )
+}
 
-export default BottomTabNavigator;
+export default BottomTabNavigator
+
+
